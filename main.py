f2634ddb52ebe456bf09ccb6e3680f4f2ad9382f
saisrujan12 <katakamsaisrujanreddy@gmail.com>
2026-07-06 12:17:44 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+import time
+import uuid
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.base import BaseHTTPMiddleware
+
+EMAIL = "23f2003943@ds.study.iitm.ac.in"
+
+ALLOWED_ORIGIN = "https://dash-kac954.example.com"
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[ALLOWED_ORIGIN],
+    allow_methods=["GET", "OPTIONS"],
+    allow_headers=["*"],
+)
+
+class MetricsMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next):
+        start = time.perf_counter()
+
+        response = await call_next(request)
+
+        process_time = time.perf_counter() - start
+
+        response.headers["X-Request-ID"] = str(uuid.uuid4())
+        response.headers["X-Process-Time"] = str(process_time)
+
+        return response
+
+app.add_middleware(MetricsMiddleware)
+
+@app.get("/stats")
+def stats(values: str = Query(...)):
+    nums = [int(x) for x in values.split(",")]
+
+    return {
+        "email": EMAIL,
+        "count": len(nums),
+        "sum": sum(nums),
+        "min": min(nums),
+        "max": max(nums),
+        "mean": sum(nums)/len(nums)
+    }
+
+    app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://dash-kac954.example.com"],
+    allow_credentials=False,
+    allow_methods=["GET", "OPTIONS"],
+    allow_headers=["*"],
+)
